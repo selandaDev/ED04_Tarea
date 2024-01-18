a8d59efe803e2d9f2199b52519c1ecfb917ed97a
selandaDev <fmanosa23@gmail.com>
2024-01-18 14:24:02 +0100

    primer commit

--- src/ed_tarea04/Main.java
+++ src/ed_tarea04/Main.java
@@ -1,32 +1,38 @@
 package ed_tarea04;
+
 import cuentas.CCuenta;
 
 public class Main {
-    public static void main(String[] args) {
-        CCuenta cuenta1;
-        double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es " + saldoActual );
+  public static void main(String[] args) {
+    CCuenta cuenta1;
+    double saldoActual;
 
-        operativaCuenta(cuenta1, 2300, "retirar");
-        operativaCuenta(cuenta1, 695, "ingresar");
+    cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+    saldoActual = cuenta1.estado();
+    System.out.println("El saldo actual es " + saldoActual);
+
+    operativaCuenta(cuenta1, 2300, "retirar");
+    operativaCuenta(cuenta1, 695, "ingresar");
+  }
+
+  public static void operativaCuenta(
+    CCuenta cuenta,
+    double cantidad,
+    String operacion
+  ) {
+    try {
+      if (operacion.equals("retirar")) {
+        cuenta.retirar(cantidad);
+        System.out.println("Retirada completada");
+      } else if (operacion.equals("ingresar")) {
+        cuenta.ingresar(cantidad);
+        System.out.println("Ingreso completado");
+      } else {
+        System.out.println("Operación no válida");
+      }
+    } catch (Exception e) {
+      System.out.println("Fallo al " + operacion);
     }
-
-    public static void operativaCuenta(CCuenta cuenta, double cantidad, String operacion) {
-        try {
-            if (operacion.equals("retirar")) {
-                cuenta.retirar(cantidad);
-                System.out.println("Retirada completada");
-            } else if (operacion.equals("ingresar")) {
-                cuenta.ingresar(cantidad);
-                System.out.println("Ingreso completado");
-            } else {
-                System.out.println("Operación no válida");
-            }
-        } catch (Exception e) {
-            System.out.println("Fallo al " + operacion);
-        }
-    }
+  }
 }
